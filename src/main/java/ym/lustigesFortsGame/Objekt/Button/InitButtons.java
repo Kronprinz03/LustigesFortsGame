d6c6250569fcf4769d7711aa525bb156c5798cb4
yannis <yannis.e.moser@gmail.com>
2022-04-07 14:24:02 +0200

    Initial commit

--- src/main/java/ym/lustigesFortsGame/Objekt/Button/InitButtons.java
+++ src/main/java/ym/lustigesFortsGame/Objekt/Button/InitButtons.java
@@ -3,7 +3,7 @@ package ym.lustigesFortsGame.Objekt.Button;
 public class InitButtons {
 
     public InitButtons(){
-            ButtonTemplate lokalButton = new ButtonTemplate("Lokal PvP" , 100) {
+            ButtonTemplate lokalButton = new ButtonTemplate("Lokal PvP" , 100, 3,32,324,324,true) {
                 @Override
                 void onClick() {
 
